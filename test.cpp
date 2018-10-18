169e4688e393f314785d07837480224705d9cd08
Remi de Ferrieres <deferrieresremi@gmail.com>
2018-10-17 21:05:24 -0700

    end of lab-02

--- test.cpp
+++ test.cpp
@@ -7,6 +7,27 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+TEST(EchoTest, EmptyString) {
+    char* test_val[1]; test_val[0] = "./c-echo";
+    EXPECT_EQ("", echo(1,test_val));
+}
+
+
+TEST(EchoTest, NetID) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "rdef003";
+    EXPECT_EQ("rdef003", echo(2,test_val));
+}
+
+TEST(EchoTest, Name ) {
+    char* test_val[4]; test_val[0] = "./c-echo"; test_val[1] = "Remi"; test_val[2] = "de"; test_val[3] = "Ferrieres";
+    EXPECT_EQ("Remi de Ferrieres", echo(4,test_val));
+}
+
+TEST(EchoTest, EmailAddress) {
+    char* test_val[2]; test_val[0] = "./c-echo"; test_val[1] = "deferrieresremi@gmail.com";
+    EXPECT_EQ("deferrieresremi@gmail.com", echo(2,test_val));
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
